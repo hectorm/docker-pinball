626612f64d7dbd6c420d4d607711509ac98ee97b
Héctor Molinero Fernández <hector@molinero.dev>
2019-06-01 17:07:52 +0200

    Made configurable image registry and small improvements in the building system

--- run.sh
+++ run.sh
@@ -3,36 +3,36 @@
 set -eu
 export LC_ALL=C
 
-DOCKER_IMAGE_NAMESPACE=hectormolinero
-DOCKER_IMAGE_NAME=pinball
-DOCKER_IMAGE_VERSION=latest
-DOCKER_IMAGE=${DOCKER_IMAGE_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
-DOCKER_CONTAINER=${DOCKER_IMAGE_NAME}
-DOCKER_VOLUME="${DOCKER_CONTAINER}"-data
+IMAGE_NAMESPACE=hectormolinero
+IMAGE_PROJECT=pinball
+IMAGE_TAG=latest
+IMAGE_NAME=${IMAGE_NAMESPACE}/${IMAGE_PROJECT}:${IMAGE_TAG}
+CONTAINER_NAME=${IMAGE_PROJECT}
+VOLUME_NAME=${CONTAINER_NAME}-data
 
 imageExists() { [ -n "$(docker images -q "$1")" ]; }
 containerExists() { docker ps -aqf name="$1" --format '{{.Names}}' | grep -Fxq "$1"; }
 containerIsRunning() { docker ps -qf name="$1" --format '{{.Names}}' | grep -Fxq "$1"; }
 
-if ! imageExists "${DOCKER_IMAGE}"; then
-	>&2 printf -- '%s\n' "${DOCKER_IMAGE} image doesn't exist!"
+if ! imageExists "${IMAGE_NAME}"; then
+	>&2 printf -- '%s\n' "\"${IMAGE_NAME}\" image doesn't exist!"
 	exit 1
 fi
 
-if containerIsRunning "${DOCKER_CONTAINER}"; then
-	printf -- '%s\n' "Stopping \"${DOCKER_CONTAINER}\" container..."
-	docker stop "${DOCKER_CONTAINER}" >/dev/null
+if containerIsRunning "${CONTAINER_NAME}"; then
+	printf -- '%s\n' "Stopping \"${CONTAINER_NAME}\" container..."
+	docker stop "${CONTAINER_NAME}" >/dev/null
 fi
 
-if containerExists "${DOCKER_CONTAINER}"; then
-	printf -- '%s\n' "Removing \"${DOCKER_CONTAINER}\" container..."
-	docker rm "${DOCKER_CONTAINER}" >/dev/null
+if containerExists "${CONTAINER_NAME}"; then
+	printf -- '%s\n' "Removing \"${CONTAINER_NAME}\" container..."
+	docker rm "${CONTAINER_NAME}" >/dev/null
 fi
 
 if [ -d '/tmp/.X11-unix' ]; then
 	X11_SOCKET_DIRECTORY='/tmp/.X11-unix'
 
-	XAUTHORITY_FILE="/tmp/.Xauthority.docker.${DOCKER_IMAGE_NAME}"
+	XAUTHORITY_FILE="/tmp/.Xauthority.docker.${IMAGE_PROJECT}"
 	touch "${XAUTHORITY_FILE}"
 	xauth nlist "${DISPLAY}" | sed -e 's/^..../ffff/' | xauth -f "${XAUTHORITY_FILE}" nmerge -
 fi
@@ -41,13 +41,13 @@ if [ -S "${XDG_RUNTIME_DIR-}/pulse/native" ]; then
 	PULSEAUDIO_SOCKET="${XDG_RUNTIME_DIR-}/pulse/native"
 fi
 
-printf -- '%s\n' "Creating \"${DOCKER_CONTAINER}\" container..."
+printf -- '%s\n' "Creating \"${CONTAINER_NAME}\" container..."
 exec docker run --tty --interactive --rm \
-	--name "${DOCKER_CONTAINER}" \
-	--hostname "${DOCKER_CONTAINER}" \
+	--name "${CONTAINER_NAME}" \
+	--hostname "${CONTAINER_NAME}" \
 	--network none \
 	--log-driver none \
-	--mount type=volume,src="${DOCKER_VOLUME}",dst='/home/wine/.wine' \
+	--mount type=volume,src="${VOLUME_NAME}",dst='/home/wine/.wine' \
 	${X11_SOCKET_DIRECTORY+ \
 		--env DISPLAY="${DISPLAY}" \
 		--mount type=bind,src="${X11_SOCKET_DIRECTORY}",dst='/tmp/.X11-unix',ro \
@@ -58,4 +58,4 @@ exec docker run --tty --interactive --rm \
 		--env PULSE_SERVER='/run/user/1000/pulse/native' \
 		--mount type=bind,src="${PULSEAUDIO_SOCKET}",dst='/run/user/1000/pulse/native',ro \
 	} \
-	"${DOCKER_IMAGE}" "$@"
+	"${IMAGE_NAME}" "$@"
